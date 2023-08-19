2d6b078b1d8ca8f85785bd71bb7d43c2dc672f28
Edon <edon@valdman.works>
2023-08-19 11:52:21 -0500

    Renamed test target name

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
             name: "AVCaptureViewModel",
             dependencies: []),
         .testTarget(
-            name: "CameraViewModelTests",
+            name: "AVCaptureViewModelTests",
             dependencies: ["AVCaptureViewModel"]),
     ]
 )
