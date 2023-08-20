343b2caf54900b6357578735f7f355950f18402c
Edon <edon@valdman.works>
2023-08-20 15:48:12 -0500

    Update AVCaptureViewModel+CapturingPhotos.swift
    Updated note to self

--- Sources/AVCaptureViewModel/ViewModel+Extensions/AVCaptureViewModel+CapturingPhotos.swift
+++ Sources/AVCaptureViewModel/ViewModel+Extensions/AVCaptureViewModel+CapturingPhotos.swift
@@ -29,7 +29,7 @@ extension AVCaptureViewModel {
             photoSettings.livePhotoMovieFileURL = uniqueTemporaryDirectoryFileURL()
         }
         
-        #warning("What to do with this for pre-17?")
+        #warning("What to do with this for pre-17? as in, what does it do in 17?")
         // Start tracking capture readiness on the main thread to synchronously
         // update the shutter button's availability.
 //        if #available(iOS 17.0, *) {
